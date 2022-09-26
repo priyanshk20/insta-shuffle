6ee6e9961236d8ba1b1b8959b2b8028e3def3b78
priyanshk20 <khandelwalpriyansh85@gmail.com>
2022-09-27 01:36:42 +0530

    added basic UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ScreenComponent } from './screen/screen.component';
+import { ProfileCardComponent } from './profile-card/profile-card.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScreenComponent,
+    ProfileCardComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
